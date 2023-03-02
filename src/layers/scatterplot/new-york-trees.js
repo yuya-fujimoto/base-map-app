5c7df1aa8734aa1563de0cd9379d2183eb6eff6f
Yuya Fujimoto <yuya.fujimoto.88@gmail.com>
2023-03-02 15:58:16 -0800

    add new york tree scatterplot

---
+++ src/layers/scatterplot/new-york-trees.js
@@ -0,0 +1,29 @@
+import { ScatterplotLayer } from '@deck.gl/layers';
+
+const initialViewState = {
+    longitude:  -73.982302,
+    latitude: 40.760306,
+    zoom: 12,
+    pitch: 0,
+    bearing: 0
+};
+
+const layer = new ScatterplotLayer({
+    id: 'scatterplot-tree-layer',
+    data: 'https://data.cityofnewyork.us/resource/5rq2-4hqu.json?$limit=65000&&boroname=Manhattan',
+    getPosition: d => d.the_geom.coordinates,
+    getFillColor: d => [51, 255, 60],
+    getLineColor: d => [0, 0, 0],
+    opacity: 0.8,
+    stroked: true,
+    filled: true,
+    radiusScale: 6,
+    radiusMinPixels: 1,
+    radiusMaxPixels: 100,
+    lineWidthMinPixels: 1    
+});
+
+export const NewYorkTreesLayer = {
+    layer,
+    initialViewState
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Map from 'react-map-gl'
+import DeckGL from '@deck.gl/react';
+import { NewYorkTreesLayer } from '../layers/scatterplot/new-york-trees'
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoieXV5YWZ1amltb3RvIiwiYSI6ImNsZWdyZDA4NTA1ZGwzeG53Y2c2OGY0bjAifQ._mbKMYj_moCfX1GcJmxAJg'
 
@@ -16,16 +16,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Map
-          initialViewState={{
-            longitude: -122.4,
-            latitude: 37.8,
-            zoom: 14
-          }}
-          style={{width: '100vw', height: '100vh'}}
-          mapStyle="mapbox://sprites/mapbox/bright-v8"
-          mapboxAccessToken={MAPBOX_TOKEN}
-        />
+        <DeckGL
+          initialViewState={NewYorkTreesLayer.initialViewState}
+          controller={true}
+          layers={[NewYorkTreesLayer.layer]}
+        >
+          <Map
+            style={{width: '100vw', height: '100vh'}}
+            mapStyle="mapbox://sprites/mapbox/dark-v8"
+            mapboxAccessToken={MAPBOX_TOKEN}
+          />
+        </DeckGL>
       </main>
     </>
   )
